0cb95503cb5ce20b6c91682cda0b6e86f34f2511
Ivonne131 <61197906+Ivonne131@users.noreply.github.com>
2020-11-17 21:24:12 -0600

    Update chart-area-demo.js

--- assets/demo/chart-area-demo.js
+++ assets/demo/chart-area-demo.js
@@ -7,9 +7,9 @@ var ctx = document.getElementById("myAreaChart");
 var myLineChart = new Chart(ctx, {
   type: 'line',
   data: {
-    labels: ["Mar 1", "Mar 2", "Mar 3", "Mar 4", "Mar 5", "Mar 6", "Mar 7", "Mar 8", "Mar 9", "Mar 10", "Mar 11", "Mar 12", "Mar 13"],
+    labels: ["Martes 1", "Miércoles 2", "Jueves 3", "Viernes 4", "Sábado 5", "Domingo 6", "Lunes 7", "Martes 8", "Miércoles 9", "Jueves 10", "Viernes 11", "Sábado 12", "Domingo 13"],
     datasets: [{
-      label: "Sessions",
+      label: "Porcentaje de avance",
       lineTension: 0.3,
       backgroundColor: "rgba(2,117,216,0.2)",
       borderColor: "rgba(2,117,216,1)",
@@ -20,7 +20,7 @@ var myLineChart = new Chart(ctx, {
       pointHoverBackgroundColor: "rgba(2,117,216,1)",
       pointHitRadius: 50,
       pointBorderWidth: 2,
-      data: [10000, 30162, 26263, 18394, 18287, 28682, 31274, 33259, 25849, 24159, 32651, 31984, 38451],
+      data: [0, 8, 15, 26, 32, 39, 49, 61, 67, 74, 79, 83, 92],
     }],
   },
   options: {
@@ -39,7 +39,7 @@ var myLineChart = new Chart(ctx, {
       yAxes: [{
         ticks: {
           min: 0,
-          max: 40000,
+          max: 100,
           maxTicksLimit: 5
         },
         gridLines: {
